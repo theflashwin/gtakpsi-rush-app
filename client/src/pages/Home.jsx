617c529fc5eb8eb8d91789985c9c4358ab97a461
theflashwin <theflashwin1@gmail.com>
2025-01-12 17:58:39 -0500

    Fixed Learn More Link

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -53,7 +53,9 @@ export default function Home() {
                     }} className="px-6 py-3 bg-sky-500 text-white font-semibold rounded-lg shadow-md hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                         Register for Rush
                     </button>
-                    <button className="px-6 py-3 bg-orange-300 text-white font-semibold rounded-lg shadow-md hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2">
+                    <button onClick={() => {
+                        window.location.href = 'https://gtakpsi.com/rush'
+                    }} className="px-6 py-3 bg-orange-300 text-white font-semibold rounded-lg shadow-md hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2">
                         Learn More
                     </button>
                 </div>
